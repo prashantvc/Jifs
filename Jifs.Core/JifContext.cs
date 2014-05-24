61d1a09e43bf78543349075c68b4d2b48eeb6bd8
prashantvc <pvc@outlook.com>
2014-05-24 17:08:55 +0530

    Initial commit

---
+++ Jifs.Core/JifContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Jifs.Model;
+using Newtonsoft.Json;
+
+namespace Jifs
+{
+    public class JifContext : IDisposable
+    {
+        const string BaseUrl = "http://api.giphy.com/v1/gifs/";
+        const string APIKey = "dc6zaTOxFJmzC";
+        readonly HttpClient _httpClient;
+
+        public JifContext()
+        {
+            _httpClient = new HttpClient { BaseAddress = new Uri(BaseUrl) };
+        }
+
+        public async Task<Rootobject> GetTrendingAsync()
+        {
+            string url = GetRequestUrl("trending?");
+            return await GetResponseAsyc<Rootobject>(url);
+        }
+
+        public async Task<Rootobject> SearchAsync(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return null;
+            }
+
+            string url = GetRequestUrl("search?q=" + query);
+            return await GetResponseAsyc<Rootobject>(url);
+        }
+
+        public async Task<Image> GetImageByIdAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return null;
+            }
+
+            string url = string.Format("{0}?api_key={1}", id, APIKey);
+            return await GetResponseAsyc<Image>(url);
+        }
+
+        async Task<T> GetResponseAsyc<T>(string url)
+        {
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+
+        static string GetRequestUrl(string urlPart)
+        {
+            return string.Format("{0}&api_key={1}", urlPart, APIKey);
+        }
+
+        public void Dispose()
+        {
+            if (_httpClient != null)
+            {
+                _httpClient.Dispose();
+            }
+        }
+    }
+}
---
+++ Jifs.Tests/JifContextTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Jifs.Tests
+{
+    [TestFixture]
+    public class JifContextTest
+    {
+        JifContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = new JifContext();
+        }
+
+        [Test]
+        public async Task GetDefault25TrendingItems()
+        {
+            var trends = await _context.GetTrendingAsync();
+
+            Assert.IsNotNull(trends);
+            Assert.AreEqual(25, trends.pagination.count);
+        }
+
+        [Test]
+        public async Task GivenASearchQueryGetItems()
+        {
+            var results = await _context.SearchAsync("cat");
+            Assert.IsNotNull(results);
+        }
+
+        [Test]
+        public async Task GivenAEmptySearchQueryReturnNullObject()
+        {
+            var results = await _context.SearchAsync(string.Empty);
+            Assert.IsNull(results);
+        }
+
+        [Test]
+        public async Task GivenAnIdGetImageObject()
+        {
+            var image = await _context.GetImageByIdAsync("feqkVgjJpYtjy");
+            Assert.IsNotNull(image);
+        }
+
+        [Test]
+        public async Task GivenAnEmptyIdReturnNull()
+        {
+            var image = await _context.GetImageByIdAsync("");
+            Assert.IsNull(image);
+        }
+
+    [TearDown]
+        public void TearDown()
+        {
+            _context.Dispose();
+        }
+    }
+}
